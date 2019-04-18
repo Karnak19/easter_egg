adc44501c716e1ed94417d228896594f331b8028
Karnak19 <basile64.v@gmail.com>
2019-04-18 12:03:51 +0200

    creating the Layout and the Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ResponsiveLayout from "./Responsive.layout";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-  
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            {/* TODO: Add the router, then move the Layout */}
+            <ResponsiveLayout />
+         </div>
+      );
+   }
 }
 
 export default App;
---
+++ src/TheNavBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
+export default class TheNavbar extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+         isOpen: false,
+         navItems: this.props.items()
+      };
+   }
+   toggle() {
+      this.setState({
+         isOpen: !this.state.isOpen
+      });
+   }
+   render() {
+      return (
+         <div>
+            <Navbar color="light" light expand="md">
+               <NavbarBrand href="/">Easter Charts</NavbarBrand>
+               <NavbarToggler onClick={this.toggle} />
+               <Collapse isOpen={this.state.isOpen} navbar>
+                  <Nav className="ml-auto" navbar>
+                     {this.state.navItems.map((items, i) => {
+                        return (
+                           <NavItem key={i}>
+                              <NavLink tag={Link} to={items.to}>
+                                 {items.label}
+                              </NavLink>
+                           </NavItem>
+                        );
+                     })}
+                  </Nav>
+               </Collapse>
+            </Navbar>
+         </div>
+      );
+   }
+}
